61297acef44cddaa0a393dbd8c27e9cf931fba2f
Lucas DeVargas Souza <ls18017@gmail.com>
2019-08-07 20:37:21 -0300

    Criado aplicação inicial!

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  title: "Contador de pessoas",
+  // debugShowCheckedModeBanner: false,
+  home: Container(
+    color: Colors.white,
+  ),
+));
